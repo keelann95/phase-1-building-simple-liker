f08fd1a3bc399e39186d56c5f9fccfd2f194c8ab
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-07-18 17:07:27 +0300

    modified main.js

--- main.js
+++ main.js
@@ -4,8 +4,38 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// Create a hidden modal immediately
+const modal = document.createElement('div');
+modal.classList.add('hidden');
+modal.id = 'modal';
+modal.textContent = 'Error!';
+document.body.appendChild(modal);
 
-
+document.addEventListener('DOMContentLoaded', () => {
+  // Add event listeners to like buttons
+  document.querySelectorAll('.like-glyph').forEach(glyph => {
+    glyph.addEventListener('click', (e) => {
+      mimicServerCall()
+        .then(() => {
+          if (e.target.textContent === EMPTY_HEART) {
+            e.target.textContent = FULL_HEART;
+            e.target.classList.add('activated-heart');
+          } else {
+            e.target.textContent = EMPTY_HEART;
+            e.target.classList.remove('activated-heart');
+          }
+        })
+        .catch(error => {
+          const modal = document.getElementById('modal');
+          modal.textContent = error;
+          modal.classList.remove('hidden');
+          setTimeout(() => {
+            modal.classList.add('hidden');
+          }, 5000);
+        });
+    });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
